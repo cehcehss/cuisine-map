5bc4ae966d0ba828b11b2e97b5854d12a0422eb2
elsiehsieh <8chingen8@gmail.com>
2018-05-26 19:43:45 +0800

    調整code順序與命名

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+var collectionRef = db.collection("foodmap");
+
+//初始化地圖
 function initMap() {
 
     var myLatLng = {
@@ -118,27 +121,25 @@ function initMap() {
             icon: 'https://elsiehsieh.github.io/cuisine-map/style/images/pin.png'
         }
     };
-    //onsnapshot
-    db.collection("foodmap").onSnapshot(function (snapshot) {
+    //從資料庫中拿出資料地址轉座標畫marker
+    collectionRef.onSnapshot(function (snapshot) {
         snapshot.docChanges.forEach(function (change) {
             if (change.type === "added") {
                 var shop = change.doc.data();
-                if (shop.imgURL == null) {
-
-                    shop.imgURL = "https://firebasestorage.googleapis.com/v0/b/html-296-elsie.appspot.com/o/fork.png?alt=media&token=4bc8f964-cf0b-4989-8e45-65d1abdad2f1";
-                }
+                // if (shop.imgURL == null) {
+                //     shop.imgURL = "https://firebasestorage.googleapis.com/v0/b/html-296-elsie.appspot.com/o/fork.png?alt=media&token=4bc8f964-cf0b-4989-8e45-65d1abdad2f1";
+                // }
                 var contentString = `<h5>${shop.name}</h5>
                                     <p>${shop.content}</p>
-                                    <img src="${shop.imgURL}" width="350px">`;
+                                    <img src="${shop.imgURL}" width="300px">`;
 
                 var infowindow = new google.maps.InfoWindow({
                     content: contentString
                 });
                 var geocoder = new google.maps.Geocoder();
                 var addr = `${shop.address}`;
-
                 var marker;
-
+                //地址轉座標
                 geocoder.geocode({
                     'address': addr
                 }, function (results, status) {
@@ -146,6 +147,7 @@ function initMap() {
                     if (status == google.maps.GeocoderStatus.OK) {
 
                         var latlng = results[0].geometry.location;
+                        //將地圖中心定位到最新一筆資料
                         map.setCenter(latlng);
                         marker = new google.maps.Marker({
                             position: latlng,
@@ -167,36 +169,43 @@ function initMap() {
 
 }
 
-var collectionRef = db.collection("foodmap");
+
 var storageRef = storage.ref();
 var file;
 
+//取得file資訊
 $("#file").change(function (e) {
     file = e.target.files[0];
-    console.log($("#file").val());
-    $(this).next('.custom-file-label').html(file.name);
-    // $('.custom-file-label').html(file.name);
-
+    $('.custom-file-label').html(file.name);
 });
-console.log(file);
 
 $("#add-btn").on("click", function () {
-    console.log($("#file").val());
-
-    getValue();
+    getValueAndSaveData();
 
 });
+var shop;
 
-function getValue() {
-    var shop;
+function getValueAndSaveData() {
     var $name = $("#name").val();
     var $address = $("#address").val();
     var $content = $("#content").val();
     var $type = $("select[id=type]").val();
     var imgURL;
 
+    //判斷有沒有上傳圖片
+    if ($("#file").val() == '') {
+        imgURL = "https://firebasestorage.googleapis.com/v0/b/html-296-elsie.appspot.com/o/food%20(1).png?alt=media&token=c6b1302b-86ee-4cd2-8ab1-08f1d8c39244";
+        addToDB();
+    } else {
+        var imageRef = storageRef.child(file.name);
+        imageRef.put(file).then(function (snapshot) {
+            console.log(snapshot.downloadURL);
+            imgURL = snapshot.downloadURL;
+            addToDB();
+        });
+    }
 
-    function saveData() {
+    function addToDB() {
         shop = {
             name: $name,
             address: $address,
@@ -210,33 +219,25 @@ function getValue() {
             .then(function (docRef) {
                 console.log("Document written with ID: ", docRef.id);
                 $(".form-control").val("");
-                alert("新增成功");
+                $('.custom-file-label').html("");
             })
             .catch(function (error) {
                 console.error("Error adding document: ", error);
             });
     }
 
-    if ($("#file").val() == '') {
-        imgURL = "https://firebasestorage.googleapis.com/v0/b/html-296-elsie.appspot.com/o/fork.png?alt=media&token=4bc8f964-cf0b-4989-8e45-65d1abdad2f1";
-        saveData();
-    } else {
-        var imageRef = storageRef.child(file.name);
-        imageRef.put(file).then(function (snapshot) {
-            console.log(snapshot.downloadURL);
-            imgURL = snapshot.downloadURL;
-            saveData();
-        });
-    }
+
     //取得shop-count集合中type文件資料
+    //新增資料後，餐廳類型統計加一
     db.collection("shop-count").doc($type)
         .get().then(function (doc) {
             if (doc.exists) {
                 var currentCount = doc.data().count;
+                currentCount = Number(currentCount);
                 currentCount += 1;
                 //update
                 db.collection("shop-count").doc($type).update({
-                        count: Number(currentCount)
+                        count: currentCount
                     })
                     .then(function () {
                         console.log("Document successfully updated!");
@@ -258,5 +259,4 @@ function getValue() {
         }).catch(function (error) {
             console.log("Error getting document:", error);
         });
-
 }
